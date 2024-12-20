5e62d39fa518dec18ee5355a7e09e20b96b5915a
SylvHardDev <hardsyldev@gmail.com>
2024-12-20 18:00:24 +0300

    a suivre

--- src/lib/fetchImages.ts
+++ src/lib/fetchImages.ts
@@ -1,2 +1,18 @@
-import type { ImageParamsResult } from "@/models/Images";
+import type { ImageResult } from "@/models/Images";
+import { ImagesSchemaWidthPhotos } from "@/models/Images";
 
+
+export default async function fetchImages(url: string):
+Promise<ImageResult>{
+    try{
+        const res = await fetch(url, {
+            headers: {
+                Authorization: process.env.PEXELS_API_KEY
+            }
+        })
+
+        if (!res.ok) throw new Error("Fetch Images error!\n")
+
+            const imagesResults: ImagesResults = await res.json()
+    }
+}
